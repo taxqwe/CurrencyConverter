0e3aab0b7f826c363b1e211696a2fdd598635912
ni.petrov <ni.petrov@qiwi.com>
2020-03-31 23:24:30 +0300

    ft(main): keyboard log

--- app/src/main/java/com/taxqwe/currencyconverter/converter/ConverterFragment.kt
+++ app/src/main/java/com/taxqwe/currencyconverter/converter/ConverterFragment.kt
@@ -1,6 +1,40 @@
 package com.taxqwe.currencyconverter.converter
 
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.taxqwe.currencyconverter.R
+import kotlinx.android.synthetic.main.fragment_converter.view.*
+import kotlinx.android.synthetic.main.layout_keyboard.view.*
 
-class ConverterFragment : Fragment(R.layout.fragment_converter)
+class ConverterFragment : Fragment(R.layout.fragment_converter) {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = super.onCreateView(inflater, container, savedInstanceState)
+        view?.let { initButtons(it) }
+        return view
+    }
+
+    private fun initButtons(view: View) {
+        view.btn0.setOnClickListener { Log.d("btn", "0") }
+        view.btn1.setOnClickListener { Log.d("btn", "1") }
+        view.btn2.setOnClickListener { Log.d("btn", "2") }
+        view.btn3.setOnClickListener { Log.d("btn", "3") }
+        view.btn4.setOnClickListener { Log.d("btn", "4") }
+        view.btn5.setOnClickListener { Log.d("btn", "5") }
+        view.btn6.setOnClickListener { Log.d("btn", "6") }
+        view.btn7.setOnClickListener { Log.d("btn", "7") }
+        view.btn8.setOnClickListener { Log.d("btn", "8") }
+        view.btn9.setOnClickListener { Log.d("btn", "9") }
+        view.btnAc.setOnClickListener { Log.d("btn", "AC") }
+        view.btnBack.setOnClickListener { Log.d("btn", "back") }
+        view.btnDot.setOnClickListener { Log.d("btn", "dot") }
+    }
+}
